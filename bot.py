ee11d6803a8af82ba7d4a82697908150238b86b2
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-06-29 21:23:59 +0000

    bug fixes

--- bot.py
+++ bot.py
@@ -11,23 +11,29 @@ log = logging.getLogger()
 # Load environment variables from .env file
 load_dotenv()
 
-# Your Twitter API credentials from environment variables
+# credentials from environment variables
 bearer_token = os.getenv('BEARER_TOKEN')
 consumer_key = os.getenv('API_KEY')
 consumer_secret = os.getenv('API_SECRET')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-
+# Authentication
 client = tweepy.Client(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret,
     access_token=access_token,
     access_token_secret=access_token_secret
 )
-log.info("Authenticated with Twitter as user")
 
-response = client.create_tweet(
-    text="This Tweet was Tweeted using Tweepy and Twitter API v2!"
-)
-print(f"https://twitter.com/user/status/{response.data['id']}")
+
+
+def create_tweet(tweet):
+    try:
+        response = client.create_tweet(text=tweet)
+        tweet_url = f"https://twitter.com/user/status/{response.data['id']}"
+        log.info(f"Tweet posted successfully: {tweet_url}")
+        return tweet_url
+    except tweepy.TweepError as e:
+        log.error(f"Error creating tweet: {e}")
+        raise SystemExit(f"Error creating tweet: {e}")
